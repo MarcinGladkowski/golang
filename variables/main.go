9c69e7cd2d64b6f92f7469be544cdc5b6be6bbbe
marcingladkowski <marcingladkowski@gmail.com>
2025-06-25 15:29:23 +0200

    variables

---
+++ variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	var x int = 5
+	var y float64 = 2.4
+	z := 10
+
+	sumSameType := x + z
+	sumConvertType := float64(x) + y
+
+	const pi float64 = 3.14
+	const hello = "Hello"
+
+}
